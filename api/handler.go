70bc9678b3179a05d0abb886f0ebd9c2a3cb1940
Ryan Gao <67180069+toblrne@users.noreply.github.com>
2023-11-04 01:05:05 -0700

    basic functionality with bytes and json

---
+++ api/handler.go
@@ -0,0 +1,80 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/toblrne/ZephyrusDBv2/db"
+)
+
+type Handler struct {
+	driver *db.Driver
+}
+
+func NewHandler(driver *db.Driver) *Handler {
+	return &Handler{driver: driver}
+}
+
+func (h *Handler) PutValue(c *gin.Context) {
+	key := c.Param("key")
+
+	// Read the content type of the incoming request
+	contentType := c.GetHeader("Content-Type")
+
+	var value []byte
+	var err error
+
+	// If the content type is JSON, use the JSON binding
+	if contentType == "application/json" {
+		var jsonValue interface{}
+		if err = c.BindJSON(&jsonValue); err == nil {
+			value, err = db.MarshalJson(jsonValue)
+		}
+	} else {
+		// For all other content types, read the body as raw bytes
+		value, err = c.GetRawData()
+	}
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid value"})
+		return
+	}
+
+	err = h.driver.Put(key, value)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusOK)
+}
+
+func (h *Handler) GetValue(c *gin.Context) {
+	key := c.Param("key")
+	value, err := h.driver.Get(key)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Respond with the content type that the value is stored in
+	contentType := http.DetectContentType(value)
+	if contentType == "application/json" {
+		// If the content type is JSON, send it as JSON
+		c.Data(http.StatusOK, contentType, value)
+	} else {
+		// Otherwise, send it as raw data
+		c.Data(http.StatusOK, "application/octet-stream", value)
+	}
+}
+
+func (h *Handler) DeleteValue(c *gin.Context) {
+	key := c.Param("key")
+	err := h.driver.Delete(key)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusOK)
+}
---
+++ api/router.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// InitRouter initializes and returns the Gin Engine with configured routes
+func InitRouter(handler *Handler) *gin.Engine {
+	router := gin.Default()
+
+	// Define your API endpoints here
+	router.PUT("/key/:key", handler.PutValue)
+	router.GET("/key/:key", handler.GetValue)
+	router.DELETE("/key/:key", handler.DeleteValue)
+
+	return router
+}
